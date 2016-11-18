78944f897576931a1bc8fcde33f757bf97c969da
Starian Chen (Shang Hai Wei Chuang Ruan Jian) <v-stache@microsoft.com>
2016-11-18 15:40:00 +0800

    sds

--- ClassLibrary1/C/GitTest.bat
+++ ClassLibrary1/C/GitTest.bat
@@ -1,3 +1,5 @@
+git config --get remote.origin.url
+
 git remote add origin http.https://starain.visualstudio.com/_git/GtitTest
 
 git config --get-all http.https://starain.visualstudio.com/_git/GtitTest.extraheader
