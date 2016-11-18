aa0c6dd8ca2a98bf83c5a133e02b99c4f601c742
Starian Chen (Shang Hai Wei Chuang Ruan Jian) <v-stache@microsoft.com>
2016-11-18 15:18:47 +0800

    dd

---
+++ ClassLibrary1/GitTest.bat
@@ -0,0 +1,13 @@
+﻿git version
+
+git config --get remote.origin.url
+
+git config gc.auto 0
+
+git config --get-all http.https://starain.visualstudio.com/_git/GtitTest.extraheader
+
+git -c http.extraheader="AUTHORIZATION: bearer %1" fetch --tags --prune --progress origin
+
+git checkout --progress --force master
+
+git config http.https://starain.visualstudio.com/_git/GtitTest.extraheader "AUTHORIZATION: bearer %1"
